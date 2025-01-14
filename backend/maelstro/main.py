1e32e1be50f22a523e0006ef171e28c806d0d96d
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2025-01-14 15:42:04 +0100

    setup project with poetry requirement management, CI, and code checks

--- backend/maelstro/main.py
+++ backend/maelstro/main.py
@@ -1,16 +1,29 @@
+"""
+Main backend app setup
+"""
+
 from fastapi import FastAPI, Response
+
 
 app = FastAPI()
 
 app.state.health_countdown = 5
 
+
 @app.get("/")
 def root_page():
+    """
+    Hello world dummy route
+    """
     return {"Hello": "World"}
 
 
 @app.get("/health")
 def health_check(response: Response):
+    """
+    Health check to make sure the server is up and running
+    For test purposes, the server is reported healthy only from the 5th request onwards
+    """
     status: str = "healthy"
     if app.state.health_countdown > 0:
         app.state.health_countdown -= 1
