d74f0aab165ac2c11db8ae1aecae4b72f75d5eae
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2025-01-15 10:14:39 +0100

    fix CI

--- backend/maelstro/gn_session.py
+++ backend/maelstro/gn_session.py
@@ -1,6 +1,7 @@
 import requests
 from typing import BinaryIO
 
+
 class GNSession:
     def __init__(self, apiUrl: str, user: str, pw: str):
         self._apiUrl = apiUrl
@@ -9,35 +10,86 @@ class GNSession:
         self._session = requests.Session()
         # only with gateway ...
         # self._session.post(f"{gateway_url}/login", data={"username": user, "password": pw})
-        r = self._session.get(f"{self._apiUrl}/me", headers={"Accept": "application/json"}, auth=(user, pw))
+        r = self._session.get(
+            f"{self._apiUrl}/me",
+            headers={"Accept": "application/json"},
+            auth=(user, pw),
+        )
         if r.status_code == 200:
             jj = r.json()
-            print(f"Connected as {jj['name']} {jj['surname']} with profile {jj['profile']}")
+            print(
+                f"Connected as {jj['name']} {jj['surname']} with profile {jj['profile']}"
+            )
         else:
-            r = self._session.get(f"{self._apiUrl}/me", headers={"Accept": "application/json"})
+            r = self._session.get(
+                f"{self._apiUrl}/me", headers={"Accept": "application/json"}
+            )
             if r.status_code == 204:
                 print("Not connected")
             else:
-                raise Exception(f"Error in basic auth - status_code: {r.status_code} - error: {r.text}")
+                raise ConnectionError(
+                    f"Error in basic auth - status_code: {r.status_code} - error: {r.text}"
+                )
 
     def getUserInfo(self):
-        return self._session.get(f"{self._apiUrl}/me", headers={"Accept": "application/json"}).json()
+        return self._session.get(
+            f"{self._apiUrl}/me", headers={"Accept": "application/json"}
+        ).json()
 
     def getRecordIds(self):
-        return [i['_id'] for i in self._session.post(f"{self._apiUrl}/search/records/_search", headers={"Accept": "application/json"}, json={}).json()['hits']['hits']]
+        return [
+            i["_id"]
+            for i in self._session.post(
+                f"{self._apiUrl}/search/records/_search",
+                headers={"Accept": "application/json"},
+                json={},
+            ).json()["hits"]["hits"]
+        ]
 
     def getRecordZip(self, uuid: str):
-        r = self._session.get(f"{self._apiUrl}/records/{uuid}", headers={"Accept": "application/zip"})
+        r = self._session.get(
+            f"{self._apiUrl}/records/{uuid}", headers={"Accept": "application/zip"}
+        )
         if r.status_code == 200:
             return r.content
 
     def getRecordXml(self, uuid: str):
-        r = self._session.get(f"{self._apiUrl}/records/{uuid}", headers={"Accept": "application/xml"})
+        r = self._session.get(
+            f"{self._apiUrl}/records/{uuid}", headers={"Accept": "application/xml"}
+        )
         if r.status_code == 200:
             return r.content
 
     def uploadRecordZip(self, zipFile: BinaryIO, overwrite: bool = True):
-        return self._session.post(f"{self._apiUrl}/records", headers={"X-XSRF-TOKEN": self._session.cookies.get('XSRF-TOKEN', path='/geonetwork'), "Accept": "application/json"}, files={"file": ("file.zip", zipFile, "application/zip")}, params={"metadataType": "METADATA", "uuidProcessing": "OVERWRITE" if overwrite else "GENERATEUUID"}, auth=(self._user, self._pw))
+        return self._session.post(
+            f"{self._apiUrl}/records",
+            headers={
+                "X-XSRF-TOKEN": self._session.cookies.get(
+                    "XSRF-TOKEN", path="/geonetwork"
+                ),
+                "Accept": "application/json",
+            },
+            files={"file": ("file.zip", zipFile, "application/zip")},
+            params={
+                "metadataType": "METADATA",
+                "uuidProcessing": "OVERWRITE" if overwrite else "GENERATEUUID",
+            },
+            auth=(self._user, self._pw),
+        )
 
     def uploadRecordXml(self, xmlFile: BinaryIO, overwrite=True):
-        return self._session.post(f"{self._apiUrl}/records", headers={"X-XSRF-TOKEN": self._session.cookies.get('XSRF-TOKEN', path='/geonetwork'), "Accept": "application/json"}, files={"file": ("file.xml", xmlFile, "application/zip")}, params={"metadataType": "METADATA", "uuidProcessing": "OVERWRITE" if overwrite else "GENERATEUUID"}, auth=(self._user, self._pw))
+        return self._session.post(
+            f"{self._apiUrl}/records",
+            headers={
+                "X-XSRF-TOKEN": self._session.cookies.get(
+                    "XSRF-TOKEN", path="/geonetwork"
+                ),
+                "Accept": "application/json",
+            },
+            files={"file": ("file.xml", xmlFile, "application/zip")},
+            params={
+                "metadataType": "METADATA",
+                "uuidProcessing": "OVERWRITE" if overwrite else "GENERATEUUID",
+            },
+            auth=(self._user, self._pw),
+        )
--- backend/maelstro/main.py
+++ backend/maelstro/main.py
@@ -2,17 +2,18 @@
 Main backend app setup
 """
 
-from typing import Annotated
+# from typing import Annotated
 
 from io import BytesIO
-from fastapi import FastAPI, Depends, Response
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import FastAPI, Response  # , Depends
+
+# from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from maelstro.gn_session import GNSession
 
 
-app = FastAPI(root_path='/maelstrob')
-security = HTTPBasic()
+app = FastAPI(root_path="/maelstrob")
+# security = HTTPBasic()
 
 app.state.health_countdown = 5
 
@@ -38,15 +39,28 @@ def health_check(response: Response):
         status = "unhealthy"
     return {"status": status, "user": None}
 
+
 @app.get("/record/ids")
 # def get_record_ids(credentials: Annotated[HTTPBasicCredentials, Depends(security)], server="http://gateway:8080/geonetwork/srv/api/"):
-def get_record_ids(credentials_username: str = 'testadmin', credentials_password: str = 'testadmin', server="http://gateway:8080/geonetwork/srv/api/"):
+def get_record_ids(
+    credentials_username: str = "testadmin",
+    credentials_password: str = "testadmin",
+    server="http://gateway:8080/geonetwork/srv/api/",
+):
     gnSession = GNSession(server, credentials_username, credentials_password)
     return gnSession.getRecordIds()
 
+
 @app.put("/record/{uuid}")
 # def put_record(uuid: str, credentials: Annotated[HTTPBasicCredentials, Depends(security)], source_server: str, target_server="http://gateway:8080/geonetwork/srv/api/", overwrite=True):
-def put_record(uuid: str, source_server: str, src_cred='admin', target_server="http://gateway:8080/geonetwork/srv/api", tgt_cred='testadmin', overwrite: bool = True):
+def put_record(
+    uuid: str,
+    source_server: str,
+    src_cred="admin",
+    target_server="http://gateway:8080/geonetwork/srv/api",
+    tgt_cred="testadmin",
+    overwrite: bool = True,
+):
     srcSession = GNSession(source_server, src_cred, src_cred)
     zipfile_contents = srcSession.getRecordZip(uuid)
     print(len(zipfile_contents))
