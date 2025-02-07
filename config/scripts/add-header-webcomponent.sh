ec0633ab300b01d10164399e71eb87344152caba
Jean-Michel Crepel <jean-michel.crepel@camptocamp.com>
2025-02-07 15:36:40 +0100

    fix typo

--- config/scripts/add-header-webcomponent.sh
+++ config/scripts/add-header-webcomponent.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# To be properl executed by datahub initialization need the +x flag 
+# To be properl executed by maelstro initialization need the +x flag 
 # so you need to add it with chmod and commit/push it
 
 MAELSTRO_DIR=${1:-/app}
