f51e1d0deec2ad0d96badb44f00300f91e8486ac
Jean-Michel Crepel <jean-michel.crepel@camptocamp.com>
2025-02-07 15:27:00 +0100

    create config folder

---
+++ config/README.md
@@ -0,0 +1,5 @@
+# configuration 
+
+this folder contains the configurations files for maelstro
+
+we usually copy it into georchestra datadir
---
+++ config/scripts/add-header-webcomponent.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# To be properl executed by datahub initialization need the +x flag 
+# so you need to add it with chmod and commit/push it
+
+MAELSTRO_DIR=${1:-/app}
+SNIPPET="<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header active-app='maelstro' style='height:90px'></geor-header>"
+
+if grep -q "${SNIPPET}" "${MAELSTRO_DIR}/index.html"; then
+  echo "[INFO] geOrchestra: header already present."
+  exit 0
+fi
+
+echo "[INFO] geOrchestra: adding header in the main page..."
+sed -i "s#<body>#<body>${SNIPPET}#" ${MAELSTRO_DIR}/index.html
