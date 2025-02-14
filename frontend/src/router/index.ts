de0ad721a3e051ba39ca0d8c7be5eb60e262b429
Arnaud Morvan <arnaud.morvan@camptocamp.com>
2025-02-14 16:24:15 +0100

    Remove files from VueJS project template (#41)

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -22,14 +22,6 @@ const router = createRouter({
           name: 'synchronize',
           component: FormView,
         },
-        {
-          path: 'about',
-          name: 'about',
-          // route level code-splitting
-          // this generates a separate chunk (About.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import('../views/AboutView.vue'),
-        },
       ],
     },
   ],
