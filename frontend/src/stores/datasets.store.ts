45b03e0d442ee65cf0dc43052c9f969fd5555f48
Arnaud Morvan <arnaud.morvan@camptocamp.com>
2025-02-27 14:10:36 +0100

    Allow selecting harvested datasets (#59)

--- frontend/src/stores/datasets.store.ts
+++ frontend/src/stores/datasets.store.ts
@@ -8,9 +8,14 @@ export const useDatasetsStore = defineStore('datasets', () => {
   const loading = ref(false)
 
   // Actions
-  const search = async (sourceName: string, query: string): Promise<void> => {
+  const search = async (
+    sourceName: string,
+    query: string,
+    includeHarvested: boolean,
+  ): Promise<void> => {
     loading.value = true
-    results.value = (await geonetworkSearchService.search(sourceName, query)) || []
+    results.value =
+      (await geonetworkSearchService.search(sourceName, query, includeHarvested)) || []
     loading.value = false
   }
 
