5c2f2c2515361c73e8750bdc42f8b051ee965f3d
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2025-03-04 16:00:32 +0100

    Add links to metadata

--- frontend/src/views/LogsView.vue
+++ frontend/src/views/LogsView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import LogsReport from '@/components/LogsReport.vue'
 import type { Log } from '@/services/logs.service'
+import { useConfigStore } from '@/stores/config.store'
 import { useLogsStore } from '@/stores/logs.store'
 import { Column, DataTable, Paginator, Tag } from 'primevue'
 import { ref } from 'vue'
 
+const configStore = useConfigStore()
 const logsStore = useLogsStore()
 
 const getSeverity = (log: Log) => {
@@ -26,6 +28,16 @@ const onRowsChange = (value: number) => {
 }
 
 const expandedRows = ref({})
+
+const getRowMetadataUrl = (data: Log): string => {
+  try {
+    const source = configStore.getSourceByName(data.src_name)
+    return configStore.getMetadataUrl(source!, data.dataset_uuid)
+  } catch (error) {
+    console.error('Error while getting metadata URL', error)
+    return ''
+  }
+}
 </script>
 
 <template>
@@ -77,7 +89,12 @@ const expandedRows = ref({})
           class="max-w-60 overflow-hidden text-ellipsis whitespace-nowrap"
           :title="slotProps.data.src_title"
         >
-          {{ slotProps.data.src_title }}
+          <a
+            :href="getRowMetadataUrl(slotProps.data)"
+            target="_blank"
+            class="text-blue-600 dark:text-blue-500 hover:underline"
+            >{{ slotProps.data.src_title }}</a
+          >
         </div>
       </template>
     </Column>
